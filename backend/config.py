601fe4f02ffd9c6bd510d347ddae43f83c8642f1
Saroosh-Ahmed <94124272+Saroosh-Ahmed@users.noreply.github.com>
2023-12-05 16:59:47 +0500

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:16199@localhost/cms'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:"Your_password"@localhost/cms'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestingConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:16199@localhost/cms_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:"Your_password"@localhost/cms_test'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
